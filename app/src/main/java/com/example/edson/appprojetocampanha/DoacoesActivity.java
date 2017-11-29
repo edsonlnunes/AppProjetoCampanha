eb1f1fb300552b5c25fdb7ea3aa3fe5bc216a50f
Edson Lucas <edsonmartins3110@gmail.com>
2017-11-29 03:01:08 -0200

    Ajustado a parte das afinidades. Implementado o Recuperar senha (Falta alguns ajustes). Implementado o começo da tela de Doações

--- app/src/main/java/com/example/edson/appprojetocampanha/DoacoesActivity.java
+++ app/src/main/java/com/example/edson/appprojetocampanha/DoacoesActivity.java
@@ -9,5 +9,7 @@ public class DoacoesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doacoes);
+
+        String teste = "teste";
     }
 }
